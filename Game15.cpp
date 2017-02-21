60cd83f8d18423735832c060fef0a7c4b98ad859
jr192 <joao_mondim_16@hotmail.com>
2017-02-21 22:49:14 +0000

    Add files via upload

--- Game15.cpp
+++ Game15.cpp
@@ -7,11 +7,14 @@ struct mystruct {
     int config[MAX][MAX];
     int depth;
     int cost;
+    int posx;
+    int posy;
     mystruct *next;
     mystruct *pai;
 };
 
 mystruct *Game;
+
 void solve(char dir){
     int temp=0;
     if(dir=='w'){
@@ -25,18 +28,57 @@ void solve(char dir){
             }
         }
     }
+    if(dir=='s'){
+        for (int i=0; i<MAX; i++) {
+            for (int j=0; j<MAX; j++) {
+                if(Game->config[i][j]==0){
+                    temp=Game->config[i+1][j];
+                    Game->config[i+1][j]=Game->config[i][j];
+                    Game->config[i][j]=temp;
+                }
+            }
+        }
+    }
+    if(dir=='a'){
+        for (int i=0; i<MAX; i++) {
+            for (int j=0; j<MAX; j++) {
+                if(Game->config[i][j]==0){
+                    temp=Game->config[i][j-1];
+                    Game->config[i][j-1]=Game->config[i][j];
+                    Game->config[i][j]=temp;
+                }
+            }
+        }
+    }
+    if(dir=='d'){
+        for (int i=0; i<MAX; i++) {
+            for (int j=0; j<MAX; j++) {
+                if(Game->config[i][j]==0){
+                    temp=Game->config[i][j+1];
+                    cout << temp << " TEMP " << Game->config[i][j+1] <<endl;
+                    Game->config[i][j+1]=Game->config[i][j];
+                    cout << Game->config[i][j] << " segundo "<< endl;
+                    Game->config[i][j]=temp;
+                }
+            }
+        }
+    }
 }
 
 void read(int flag){
-    
+
     int i=0,j=0;
-    
+
     Game=(struct mystruct*)malloc(sizeof(struct mystruct));
     if(flag==1){
         int config1[MAX][MAX]={{1,2,3,4},{5,6,8,12},{13,9,0,7},{14,11,10,15}};
         for(i=0;i<MAX;i++){
             for(j=0;j<MAX;j++){
                 Game->config[i][j]=config1[i][j];
+                if(config1[i][j]==0) {
+                  Game->posx=i;
+                  Game->posy=j;
+                }
             }
         }
     }
@@ -45,6 +87,10 @@ void read(int flag){
         for(i=0;i<MAX;i++){
             for(j=0;j<MAX;j++){
                 Game->config[i][j]=config1[i][j];
+                if(config1[i][j]==0) {
+                  Game->posx=i;
+                  Game->posy=j;
+                }
             }
         }
     }
@@ -61,8 +107,8 @@ void print(){
         }
         cout << endl;
     }
-    
-    
+
+
 }
 
 int main(){
@@ -70,7 +116,7 @@ int main(){
     cin >> flag;
     read(flag);
     print();
-    
+
     return 0;
-    
+
 }
